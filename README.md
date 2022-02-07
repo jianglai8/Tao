3731d2d4ba94a64df60665e7148e009f79702d13
Lai Jiang <laii.jiang@mail.utoronto.ca>
2022-02-06 20:59:16 -0500

    readme and script updated

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ Insuffient critical care resources led to increased COVID-19 mortality in Toront
 2.Project description:
 Data about COVID-19 cases in Toronto, extracted from OpenDataToronto is utilized to analyze how insufficient medical resources including facilities and workforces resulted in low ICU admission rates and the fatal cases of COVID-19 patients. More specifically, the possible cause of the resource scarcity, the ICU admission criteria and the hospital policies implemented in Ontario in response to the drastically increased number of patients are discussed thoroughly.
 
+3.Variables 
+This report mainly focuses on the discussion of 6 variables including age group, neighbourhood name, classification, source of infection, outcome and ever in ICU.
+
 3.Table of content:
 (1) introduction
 (2) Data
@@ -17,6 +20,12 @@ Data about COVID-19 cases in Toronto, extracted from OpenDataToronto is utilized
 (3) Conclusion
 
 4.Installation:
-(1) Install R
-(2) Install packages required
+(1) Install R on the official website of R https://www.r-project.org
+(2) Install packages required by install.packages("package_name")
+(3) Run the rmd file
+(4) Click knit and knit to pdf in the rmd file to produce the output pdf
+
+5. Additional information:
+(1) The shape file used in this report can be found in the neigh folder within input folder or https://github.com/mylesmharrison/toronto_neighbourhoods. Note that the dbf file, shx file and shp file need to be downloaded together in one folder to ensure that the code runs. The path of reading the shape file needs to be updated after downloading the folder.
+(2) If the code fails to run due to the get_resource() function on the 63rd line and return Error: Internal Server Error (HTTP 500). Please try to run the code several times or restart R sessions. It will eventually succeed.
 
--- scripts/script.R
+++ scripts/script.R
@@ -1,13 +1,15 @@
 # this is my first r script
-# data is read by package opendatatoronto, stored and cleaned
-# in section 2.4, the graph of fatal cases classicified by ever in icu is plotted
+# section 2.4 Insufficient ICU Resources
+ggplot(hos, aes(x=age_group, y=n, fill=ever_in_icu))+ geom_bar(stat='identity', position='dodge') + labs(title = "Figure 1: Fatal cases classified by ever in ICU", x = "age group", y = "number of cases") + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) + guides(fill=guide_legend(title="ever in ICU")) + annotate("text", x=c(1,2,3,4,5,6,7,8,9), y=c(150,150,175,200,300,450,650,1300,1150), label = c("33%","50%","63%","59%","65%","56%","38%","14%","3%"), color = "cyan4")
 
 # this is my second r script
-# in section 2.5l the graph of source of infection of fatal cases classified by ever in ICU is plotted
-# debugging and troubleshooting the map of Toronto in section 2.6
+# section 2.5 Source of infection
+ggplot(source, aes(x=source_of_infection, y=n, fill=ever_in_icu)) + geom_bar(stat='identity', position='dodge') + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) + guides(fill=guide_legend(title="ever in ICU")) + labs(title = "Figure 2: Source of infection of fatal cases classified by ever in ICU", x = "source of infection", y = "number of cases") + annotate("text", x=c(1,2,3,4,5,6,7,8,9), y=c(200,650,265,510,170,2000,170,170,170), label = c("37%","46%","52%","40%","47%","5%","44%","50%","57%"), color = "cyan4")
 
 # this is my third r script
-# found the bug of neighbourhood id mismatch and fixed it by reasigning all ids based on the shape file
-
-# this is my fourth r script
-# constructed a table illustrating the situation of ten neighbourhoods.
+# section 2.6 Geographical Divisions
+# the map plot
+plot(sh2, col=cols, main = "Figure 3: Toronto neighbourhoods")
+gradientLegend(valRange = c(0, 81), color = p, side = 1)
+# the table plot
+knitr::kable(nei,col.names = c("Neighbourhood Name", "Ever in ICU", "Number of Confirmed Cases", "Admission Rate", "Survival Rate"), "simple", align = "lcccc", digits = 3, caption = "The top ten neighbourhoods with the highest number of ICU admitted patients")
